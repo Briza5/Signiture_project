5e8de547ee960fd65ef087746c148f8e715ba1f5
Briza5 <BrizaL555@gmail.com>
2026-01-24 22:29:24 +0100

    add new tickers to dlt pipeline, config connection between dbt and bigquery

--- ingestion/stock_pipeline.py
+++ ingestion/stock_pipeline.py
@@ -11,12 +11,13 @@ from pathlib import Path
 log_dir = Path("../logs")
 log_dir.mkdir(exist_ok=True)
 
-log_file = log_dir / f"pipeline_run_{datetime.now().strftime('%Y-%m-%d_%H-%M')}.log"
+log_file = log_dir / \
+    f"pipeline_run_{datetime.now().strftime('%Y-%m-%d_%H-%M')}.log"
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler(log_file,encoding='utf-8'),
+        logging.FileHandler(log_file, encoding='utf-8'),
         logging.StreamHandler()  # Also print to console
     ]
 )
@@ -24,11 +25,13 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Test symboly
-TEST_SYMBOLS = ["AAPL", "MSFT", "GOOGL", "AMZN", "NVDA"]
+TEST_SYMBOLS = ["AAPL", "MSFT", "GOOGL", "AMZN", "AMD", "NFLX", "INTC", "PLTR", "SNOW", "DIS", "BMY",
+                "CRM", "O"]
 
 # Global run tracking
 run_metadata = []
 run_id = str(uuid.uuid4())[:8]
+
 
 @dlt.resource(
     write_disposition="merge",
@@ -38,9 +41,9 @@ run_id = str(uuid.uuid4())[:8]
 def fetch_daily_prices(
     symbols: list,
     last_date=dlt.sources.incremental(
-        "date", 
+        "date",
         initial_value=(datetime.now() - timedelta(days=730)).date()
-       
+
     )
 ):
     """
@@ -48,28 +51,28 @@ def fetch_daily_prices(
     První run: od 2023-01-01
     Další runy: od max(date) v BigQuery
     """
-    
+
     start_from = last_date.last_value
     if isinstance(start_from, date):
         start_from = start_from.strftime("%Y-%m-%d")
 
     end_date = datetime.now().strftime("%Y-%m-%d")
-    
+
     logger.info(f"Fetching data from {start_from} to {end_date}")
-    
+
     for symbol in symbols:
         symbol_start = datetime.now()
-        
+
         try:
             logger.info(f"Downloading {symbol}...")
-            
+
             df = yf.download(
                 symbol,
                 start=start_from,
                 end=end_date,
                 progress=False
             )
-            
+
             if df.empty:
                 logger.warning(f"No new data for {symbol}")
                 run_metadata.append({
@@ -84,7 +87,7 @@ def fetch_daily_prices(
                     "data_date_range": f"{start_from} to {end_date}"
                 })
                 continue
-            
+
             # Transform
             df = df.reset_index()
 
@@ -94,12 +97,12 @@ def fetch_daily_prices(
 
             df.columns = [col.lower() for col in df.columns]
             df = df.rename(columns={"adj close": "adj_close"})
-            
+
             df["symbol"] = symbol
             df["date"] = pd.to_datetime(df["date"]).dt.date
-            
+
             rows_count = len(df)
-            
+
             # Log success
             logger.info(f"✅ {symbol}: {rows_count} rows")
             run_metadata.append({
@@ -113,9 +116,9 @@ def fetch_daily_prices(
                 "error_message": None,
                 "data_date_range": f"{start_from} to {end_date}"
             })
-            
+
             yield df.to_dict(orient="records")
-            
+
         except Exception as e:
             logger.error(f"❌ Error fetching {symbol}: {e}")
             run_metadata.append({
@@ -131,22 +134,23 @@ def fetch_daily_prices(
             })
             continue
 
+
 @dlt.resource(
     write_disposition="replace",
     name="company_metadata"
 )
 def fetch_company_metadata(symbols: list):
     """Stáhne company info"""
-    
+
     metadata = []
-    
+
     for symbol in symbols:
         logger.info(f"Fetching metadata: {symbol}...")
-        
+
         try:
             ticker = yf.Ticker(symbol)
             info = ticker.info
-            
+
             metadata.append({
                 "symbol": symbol,
                 "company_name": info.get("longName", symbol),
@@ -167,8 +171,9 @@ def fetch_company_metadata(symbols: list):
                 "country": "Unknown",
                 "updated_at": datetime.now()
             })
-    
+
     yield metadata
+
 
 @dlt.resource(
     write_disposition="append",
@@ -178,41 +183,45 @@ def log_pipeline_runs():
     """Pipeline run metadata pro monitoring"""
     yield run_metadata
 
+
 @dlt.source
 def stock_data_source(symbols: list):
     """Stock data source s metadata tracking"""
-    
+
     return [
         fetch_daily_prices(symbols),
         fetch_company_metadata(symbols),
         log_pipeline_runs()
     ]
 
+
 if __name__ == "__main__":
-    
+
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--full-refresh", action="store_true", help="Full data reload")
+    parser.add_argument("--full-refresh", action="store_true",
+                        help="Full data reload")
     args = parser.parse_args()
-    
+
     logger.info("Starting Stock Pipeline...")
     logger.info(f"Symbols: {TEST_SYMBOLS}")
     logger.info(f"Run ID: {run_id}")
-    logger.info(f"Mode: {'FULL REFRESH' if args.full_refresh else 'INCREMENTAL'}")
-    
+    logger.info(
+        f"Mode: {'FULL REFRESH' if args.full_refresh else 'INCREMENTAL'}")
+
     # Init pipeline
     pipeline = dlt.pipeline(
         pipeline_name="stocks_pipeline",
         destination="bigquery",
         dataset_name="stocks_raw"
     )
-    
-      # Run
+
+    # Run
     if args.full_refresh:
         logger.info("🔄 Running FULL REFRESH...")
         logger.info("Dropping pipeline state...")
         pipeline.drop()  # Smaže lokální state
-        
+
         # Re-init
         pipeline = dlt.pipeline(
             pipeline_name="stocks_pipeline",
@@ -221,12 +230,11 @@ if __name__ == "__main__":
         )
     else:
         logger.info("⚡ Running INCREMENTAL load...")
-        
-    
+
     load_info = pipeline.run(
-            stock_data_source(symbols=TEST_SYMBOLS)
-        )
-    
+        stock_data_source(symbols=TEST_SYMBOLS)
+    )
+
     # Results
     logger.info("="*50)
     logger.info("Pipeline Completed!")
